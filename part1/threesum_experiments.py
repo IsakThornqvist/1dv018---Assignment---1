348901e6709c7b461d6fd065daed6e9e90b93395
Isak <it222hp@student.lnu.se>
2026-09-14 22:14:29 +0200

    Add random list generation and three sum tests

--- part1/threesum_experiments.py
+++ part1/threesum_experiments.py
@@ -1,4 +1,5 @@
 import random
+from threesum_algorithms import ThreeSum
 
 
 def generate_random_list(n):
@@ -10,5 +11,16 @@ def generate_random_list(n):
 
     return result
 
-test = generate_random_list(15)
-print(test)
+three_sum = ThreeSum()
+first_list = generate_random_list(15)
+second_list = generate_random_list(15)
+third_list = generate_random_list(15)
+result_one = three_sum.threesum_brute(first_list)
+result_two = three_sum.threesum_brute(second_list)
+result_three = three_sum.threesum_brute(third_list)
+print(first_list)
+print(result_one)
+print(second_list)
+print(result_two)
+print(third_list)
+print(result_three)
