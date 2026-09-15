0c34aed2fbcdafee07d939276430188dca87a57a
Isak <it222hp@student.lnu.se>
2026-09-15 15:24:50 +0200

    rename result variable for clarity

--- part1/threesum_experiments.py
+++ part1/threesum_experiments.py
@@ -16,9 +16,9 @@ first_list = generate_random_list(15)
 second_list = generate_random_list(15)
 third_list = generate_random_list(15)
 
-result_one = three_sum.threesum_brute(first_list)
-result_two = three_sum.threesum_brute(second_list)
-result_three = three_sum.threesum_brute(third_list)
+result_one_brute = three_sum.threesum_brute(first_list)
+result_two_brute = three_sum.threesum_brute(second_list)
+result_three_brute = three_sum.threesum_brute(third_list)
 
 result_one_pointer = three_sum.threesum_pointer(first_list)
 result_two_pointer = three_sum.threesum_pointer(second_list)
@@ -27,17 +27,17 @@ result_three_pointer = three_sum.threesum_pointer(third_list)
 
 print("List 1")
 print(first_list)
-print("Brute", result_one)
+print("Brute", result_one_brute)
 print("Pointer", result_one_pointer)
 
 print("List 2")
 print(second_list)
-print("Brute", result_two)
+print("Brute", result_two_brute)
 print("Pointer", result_two_pointer)
 
 print("List 3")
 print(third_list)
-print("Brute", result_three)
+print("Brute", result_three_brute)
 print("Pointer", result_three_pointer)
 
 
