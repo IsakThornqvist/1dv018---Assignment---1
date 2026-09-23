c3d10f6f16b912df09c08dd4f804ae4961027013
Isak <it222hp@student.lnu.se>
2026-09-23 18:50:13 +0200

    implementation of selection sort method

---
+++ part2/sorting_algorithms.py
@@ -0,0 +1,15 @@
+list_for_test = [1, 9, 8, 6, 5, 3, 4, 6, 10, 2, 7,]
+
+def selection_sort(lst):
+    copy_of_list = lst.copy()
+
+    for i in range(0, len(copy_of_list)):
+        min_index = i
+        for j in range(i + 1, len(copy_of_list)):
+            if copy_of_list[j] < copy_of_list[min_index]:
+                min_index = j
+        copy_of_list[i], copy_of_list[min_index] = copy_of_list[min_index], copy_of_list[i]
+
+    return copy_of_list
+
+print(selection_sort(list_for_test))
