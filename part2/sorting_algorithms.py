a14ec95433cc92fbe26b661990847e0b67f97e2b
Isak <it222hp@student.lnu.se>
2026-09-24 11:46:00 +0200

    implementaion of bubble sort algorithm

--- part2/sorting_algorithms.py
+++ part2/sorting_algorithms.py
@@ -1,4 +1,4 @@
-list_for_test = [1, 9, 8, 6, 5, 3, 4, 6, 10, 2, 7,]
+list_for_test = [9, 1, 8, 6, 5, 3, 4, 6, 10, 2, 7,]
 
 def selection_sort(lst):
     copy_of_list = lst.copy()
@@ -13,3 +13,15 @@ def selection_sort(lst):
     return copy_of_list
 
 print(selection_sort(list_for_test))
+
+
+def bubble_sort(lst):
+    copy_of_list = lst.copy()
+
+    for i in range(0, len(copy_of_list) -1):
+        for j in range(0, len(copy_of_list) -1):
+            if copy_of_list[j] > copy_of_list[j + 1]:
+                copy_of_list[j], copy_of_list[j + 1] = copy_of_list[j + 1], copy_of_list[j]
+    return copy_of_list
+
+print(bubble_sort(list_for_test))
